ad4447a07a1e439e00c5fc928dfc38e7cfd71ee8
DevShrimali <devloper.ds@gmail.com>
2020-07-07 23:45:40 +0530

    twitter button enable

--- script.js
+++ script.js
@@ -11,12 +11,36 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();        
-        authorText.innerText = data.quoteAuthor;
+        if(data.quoteAuthor === '') {
+            authorText.innerText = 'Anynomous';
+        } else {
+            authorText.innerText = data.quoteAuthor
+        }
+        // Reduce font size for Long Quotes
+        if (data.quoteText.length > 120)
+        {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
         quoteText.innerText = data.quoteText;
 
     } catch (error) {
        getQuote();        
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl,'_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 //  On Load
 getQuote();
